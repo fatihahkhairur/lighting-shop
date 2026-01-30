bcedfca69b5a6a76f47d959c269cf0d02fbc31de
Fatihah Khairur <fatihakhairur@gmail.com>
2026-01-30 13:35:15 +0800

    Add comment

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 const body = document.body;
-const filterItems = document.querySelectorAll("#side-bar button");
-const gallery = document.getElementById("gallery-view");
-/* toggle controller */
 const modeSwitch = document.getElementById("mode-switch"); 
-const filterToggle = document.querySelector(".filter-toggle");
 const sideBar = document.getElementById("side-bar");
-
-filterToggle.addEventListener("click", () => {
-  sideBar.classList.toggle("open");
-
-  if (sideBar.classList.contains("open")) {
-    filterToggle.textContent = "− Filter";
-  } else {
-    filterToggle.textContent = "+ Filter";
-  }
-});
+const filterItems = document.querySelectorAll("#side-bar button");  // [All products btn, Wall lamps btn, Table lamps btn, Ceiling lights btn]
+const filterToggle = document.querySelector(".filter-toggle");
+const gallery = document.getElementById("gallery-view");
 
 
 const productDetail = [
@@ -46,34 +35,57 @@ const productDetail = [
 ];
 
 
+/* Clickable +Filter button for mobile size interface */
+filterToggle.addEventListener("click", () => {
+  sideBar.classList.toggle("open");
+
+  if (sideBar.classList.contains("open")) {
+    filterToggle.textContent = "− Filter";
+  } else {
+    filterToggle.textContent = "+ Filter";
+  }
+});
+
+
+// Default state
 let currentFilter = "all";
 
+// Add click behavior to EVERY filter button
 filterItems.forEach(item => {
+    // Event
     item.addEventListener("click", () => {
-        currentFilter = item.dataset.filter;  // save selection
+        // currentFilter can be: "all", "wall", "table", "ceiling"
+        // Go to this button → read its custom data → get the value stored in data-filter
+        currentFilter = item.dataset.filter;  // Save which filter was chosen. E.g., item.dataset.filter → "wall"
         
-        // remove active from all
+        // Remove active style from all buttons. Reset
         filterItems.forEach(btn => btn.classList.remove("active"));
 
-        // add active to clicked
+        // Highlight the clicked button. Apply style
         item.classList.add("active");
 
         if (currentFilter === "all") {
-            renderGallery();  // show everything
+            renderGallery();  // Render the gallery without filtering
         } else {
             renderGallery(currentFilter); // show selected category
         }
     });
 });
 
+// Set default active button for All products btn on page load
 document.querySelector('[data-filter="all"]').classList.add("active");
 
 
-// user clicks switch 
-// checkbox changes state (checked/unchecked)
-modeSwitch.addEventListener("change", () => {  // "change" event fires
-    body.classList.toggle("dark-mode"); // if body has dark-mode class, remove. if no dark-mode class, added.
+// When the switch is toggled (checkbox changes state)
+modeSwitch.addEventListener("change", () => {  
+    // State flip
+    // If body HAS class dark-mode, remove it
+    // If body DOESN'T HAS class dark-mode, add it
+    body.classList.toggle("dark-mode"); 
 
+    // Re-render
+    /* Because product images change in dark mode.
+    We need to rebuild the gallery with the correct images. */
     if (currentFilter === "all") {
         renderGallery();
     } else {
@@ -82,36 +94,40 @@ modeSwitch.addEventListener("change", () => {  // "change" event fires
 });
 
 
+// This function can receive a filter. If nothing is given → filter becomes null
 function renderGallery(filter = null) {
-    gallery.innerHTML = ""; //clear previous cards
+    // Clear old content before adding new
+    gallery.innerHTML = ""; 
 
-    productDetail.forEach(product => {
-        
-        // skip if filter doesn't match
+    // Loop through your data list
+    productDetail.forEach(product => {        
+        // Skip if filter doesn't match
+        // filter = "wall" && category of product in productDetail = "wall"
         if (filter && product.category !== filter) return;
         
-        // loop through data
-        const card = document.createElement('div');  // make an empty box
-        card.classList.add('card');  // give it a style name called card
+        // Building HTML to make an empty box
+        const card = document.createElement('div');
+        card.classList.add('card');  // Give it a class name = card for styling
 
-        // decide which image to show based on body class
-        /* connects toggle to image 
-            does <body> have class "dark-mode" ? */
+        // Check if dark mode is on 
+        /* Connects toggle to image.
+            Does <body> have class "dark-mode" ? */
         const isDark = body.classList.contains("dark-mode"); 
 
-        // shortcut if/else
+        // Shortcut if/else to pick the correct image
         const imgSrc = isDark ? product.darkImage : product.lightImage;
 
+        // Fill the card with content
         card.innerHTML = `
             <img src="${imgSrc}" alt="${product.name}">
             <h3 class="product-name">${product.name}</h3>
             <p class="product-price">${product.price}</p>
         `;
 
-        // take the finished product card and place it inside the gallery.
+        // Add card to the page
         gallery.appendChild(card);
     });
 }
 
-// initial render
+// Initial render when the page load
 renderGallery();
